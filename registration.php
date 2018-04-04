4cf182d6cc56f0ac641e6716fce6d28217a21add
dawhoo <dawhoo@yahoo.com>
2018-04-04 13:24:20 -0400

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'dawhoo_ConfigurableProducts',
+    'Dawhoo_ConfigurableProducts',
     __DIR__
 );
